dc14810e05132e54410897fc1d97aadec870e47b
antoinfive <antoinfive@gmail.com>
2016-10-15 18:24:20 -0400

    add request token to API adapter and change host

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -17,7 +17,17 @@ module Adapter
         {"Authorization" => "Bearer #{oauth_token}"}
       end
 
+      def request_token(code)
+        RestClient.post "https://learn.co/oauth/token", {
+          client_id: ENV['LEARD_CLIENT_ID', 
+          client_secret: ENV['LEARDN_CLIENT_SECRET'],
+          redirect_uri: 'http://localhost:5000/auth/learn/callback',
+          grant_type: "authorization_code",
+          code: code.to_json, 
+         :content_type => :json, :accept => :json
+        }
+
+      end
     end
   end
-end
 end
