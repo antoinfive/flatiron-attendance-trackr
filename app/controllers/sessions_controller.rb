7e9eeb7ddb77a2a963f921520e2cb000fad42e85
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-10-16 10:21:34 -0400

    remove json api adapter, remove omniauth config

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,7 @@ class SessionsController < ActionController::Base
   def create    
     current_user_payload = LearnApiAuthorizer.new(params[:code]).fetch_current_user_payload
     user = User.find_or_create_from_oauth(current_user_payload)
-    redirect_to 'http://localhost:3000/login?jwt=testingJWT'
-    # render json: {jwt: "HI"}
-    # RestClient.post('http://localhost:3000/login', {jwt: "THIS WILL BE A TOKEN one day..."}.to_json, {content_type: :json, accept: :json})
-    # RestClient.get('http://localhost:3000')
-    # RestClient.post('http://localhost:3000/login', {jwt: "THIS WILL BE A TOKEN one day..."}.to_json, {content_type: :json, accept: :json})
-    
-    # return head :no_content
-    #  post to REact from here?!?!?!?!?!?!??!?!?!?!?!?!?
+    redirect_to 'http://localhost:3000/finishLogin?jwt=testingJWT'
   end
 end
 
