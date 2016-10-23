978f1c7f8ef59288cb2c156958826290898849bd
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-10-23 10:20:01 -0400

    correct format att recs by date into an array of objects for instructor flow

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,9 +3,10 @@ class Cohort < ApplicationRecord
   has_one :schedule
 
   def attendance_records_by_date
-    recs_by_date = {}
+    recs_by_date = []
     self.schedule.attendance_records.each do |ar|
-      recs_by_date[ar.date] ? recs_by_date[ar.date] << ar : recs_by_date[ar.date] = [ar]
+      recs_by_date_with_this_date = recs_by_date.find { |recs_by_date_object| recs_by_date_object[:date] == ar.date}
+      recs_by_date_with_this_date ? recs_by_date_with_this_date[:records] << ar : recs_by_date << {date: ar.date, records: [ar]}
     end 
     recs_by_date 
   end
